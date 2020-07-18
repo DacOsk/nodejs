31fb21c623d2fde3ad678a86a0bca62df4cbf9f2
DacOsk <darios000@gmail.com>
2020-07-18 20:38:08 +0200

    Started YelpCamp app

---
+++ yelpcamp/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('landing');
+});
+
+app.get('*', (req, res) => {
+    res.render('404');
+});
+
+app.listen(3000, () => console.log('YelpCamp listening...'));
