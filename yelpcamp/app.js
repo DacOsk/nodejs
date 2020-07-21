71c59e2d44cf82b46c555d1d3f525c573ba8d1b4
DacOsk <darios000@gmail.com>
2020-07-21 20:27:53 +0200

    yelpcamp add body-parser

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -1,19 +1,22 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use(express.static('public'));
-app.set('view engine', 'pug');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.render('landing');
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+    res.render("landing");
 });
 
-app.get('/campgrounds', (req, res) => {
-    res.render('campgrounds');
+app.get("/campgrounds", (req, res) => {
+    res.render("campgrounds");
 });
 
-app.get('*', (req, res) => {
-    res.render('404');
+app.get("*", (req, res) => {
+    res.render("404");
 });
 
-app.listen(3000, () => console.log('YelpCamp listening...'));
+app.listen(3000, () => console.log("YelpCamp listening..."));
