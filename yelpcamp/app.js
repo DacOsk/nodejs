321f05539ba2dd0f2ca5aa29accb8f52983afdd8
DacOsk <darios000@gmail.com>
2020-08-24 23:13:39 +0200

    add db data to page

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -18,20 +18,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "pug");
 
-db.once("open", () => {
-    const campgroundSchema = new mongoose.Schema({
-        name: String,
-        image: String
-    });
-    const Campground = mongoose.model("Campground", campgroundSchema);
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
 });
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+/* const camp = new Campground({
+    name: "Papuk",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Arang_Kel_-_Neelum_Valley.jpg/640px-Arang_Kel_-_Neelum_Valley.jpg"
+});
+camp.save((err, camp) => {
+    if (err) return console.error(err);
+    console.log("Database initialized");
+}); */
 
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds");
+    Campground.find({}, (err, campground) => {
+        if (err) return console.error(err);
+        res.render("campgrounds", {
+            campground: campground
+        });
+    });
 });
 
 app.get("*", (req, res) => {
