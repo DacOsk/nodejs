b3eb4fe446d282270f855ba186968c401038592c
DacOsk <darios000@gmail.com>
2020-08-24 21:14:38 +0200

    add mongodb connection

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+//express server
 const app = express();
+
+//db connection
+mongoose.connect("mongodb://localhost/yelpcamp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "pug");
+
+db.once("open", () => {
+    const campgroundSchema = new mongoose.Schema({
+        name: String,
+        image: String
+    });
+    const Campground = mongoose.model("Campground", campgroundSchema);
+});
 
 app.get("/", (req, res) => {
     res.render("landing");
