0e70e0cdb56437438fb16b546adc9ced49b455c2
DacOsk <darios000@gmail.com>
2020-08-31 23:27:48 +0200

    started blog app

---
+++ blog/app.js
@@ -0,0 +1,51 @@
+const bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    express = require("express"),
+    app = express();
+
+mongoose.connect("mongodb://localhost/blog_app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+app.set("view engine", "pug");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Mongoose model config
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now }
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+// RESTful routes
+app.get("/", (req, res) => {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if (err) return console.error(err);
+        res.render("index", { blogs: blogs });
+    });
+});
+
+app.post("/blogs", (req, res) => {
+    const title = req.body.title;
+    const image = req.body.image;
+    const body = req.body.body;
+    const newBlogEntry = {
+        title: title,
+        image: image,
+        body: body
+    };
+    Blog.create(newBlogEntry, (err, newBlog) => {
+        if (err) return console.error(err);
+        res.redirect("/blogs");
+    });
+});
+
+app.listen(3000, () => console.log("Blog active!"));
