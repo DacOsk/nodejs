61aa30b9ebea2218c5ebcd92f71ba970ba66119d
DacOsk <darios000@gmail.com>
2021-01-16 18:24:23 +0100

    modified db-write

--- sqlite/models/db-write.js
+++ sqlite/models/db-write.js
@@ -2,15 +2,14 @@ const sqlite3 = require('sqlite3').verbose();
 
 module.exports.dbWrite = (data, res) => {
     let db = new sqlite3.Database('./db/test.db', err => {
-        if(err) console.error(err);
+        if (err) console.error(err);
         console.log('DB connected.');
     });
 
     const sql = 'INSERT INTO test VALUES (?, ?)';
 
     const write = db.prepare(sql);
-    write.run(data.col1, data.col2);      
-    write.finalize(err => {
+    write.run(data.col1, data.col2).finalize(err => {
         if (err) console.error(err);
         console.log('data written to DB');
     });
